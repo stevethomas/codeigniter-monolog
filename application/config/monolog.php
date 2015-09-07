4aa5bafc2b479803fefd3ab7f42307cc74a46d0e
Haoran Un <haoran@untoyou.net>
2015-09-08 09:17:49 +1000

    Add Hipchat to the list of options.

--- application/config/monolog.php
+++ application/config/monolog.php
@@ -12,7 +12,7 @@
 
 
 /* GENERAL OPTIONS */
-$config['handlers'] = array('file', 'new_relic', 'hipchat'); // valid handlers are file | new_relic
+$config['handlers'] = array('file', 'new_relic', 'hipchat'); // valid handlers are file | new_relic | hipchat
 $config['channel'] = ENVIRONMENT; // channel name which appears on each line of log
 $config['threshold'] = '4'; // 'ERROR' => '1', 'DEBUG' => '2',  'INFO' => '3', 'ALL' => '4'
 $config['introspection_processor'] = TRUE; // add some meta data such as controller and line number to log messages
